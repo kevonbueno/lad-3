d05edcb82fcfd791790fe5f58ae48f59b73edbef
kevonbueno <b.buenokevin99@hotmail.com>
2018-05-11 15:39:11 -0600

    creacion de clase Aliado extends de Personaje

--- lab#3_KevinBueno/src/lab/pkg3_kevinbueno/Personajes.java
+++ lab#3_KevinBueno/src/lab/pkg3_kevinbueno/Personajes.java
@@ -17,5 +17,85 @@ public class Personajes {
     private int hp;
     private int ad;
     private int probalidad;
-    private 
+    private int velocidad;
+
+    public Personajes() {
+    }
+
+    public Personajes(int edad, String nombre, double estatura, double peso, int hp, int ad, int probalidad, int velocidad) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.hp = hp;
+        this.ad = ad;
+        this.probalidad = probalidad;
+        this.velocidad = velocidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAd() {
+        return ad;
+    }
+
+    public void setAd(int ad) {
+        this.ad = ad;
+    }
+
+    public int getProbalidad() {
+        return probalidad;
+    }
+
+    public void setProbalidad(int probalidad) {
+        this.probalidad = probalidad;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+    
+    
 }
